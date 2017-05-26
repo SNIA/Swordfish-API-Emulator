9759c151551af47973907e3c7f58c2d1208c5cfa
Naresh kumar terli <naresh.terli@ptgindia.com>
2017-05-26 14:53:35 +0530

    Latest changes
    Latest changes

---
+++ g.py
@@ -0,0 +1,17 @@
+# Copyright Notice:
+# Copyright 2016 Distributed Management Task Force, Inc. All rights reserved.
+# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfish-Interface-Emulator/LICENSE.md
+
+# Declares global variables
+
+from flask import Flask
+from flask.ext.restful import Api
+
+# Base URI. Will get overwritten in emulator.py
+rest_base = 'base'
+
+# Create Flask server
+app = Flask(__name__)
+
+# Create RESTful API
+api = Api(app)

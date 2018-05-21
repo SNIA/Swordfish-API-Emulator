5773fdadb8f206d9206628a7a6a6d7cb9e8955da
Chandra Mouli Gorti <chandramouli.gorti@ptgindia.com>
2018-05-21 18:34:37 +0530

    Fix related to #32  DELETE  Swordfish resources

--- api_emulator/swordfish/templates/storagepools.py
+++ api_emulator/swordfish/templates/storagepools.py
@@ -36,8 +36,8 @@ from flask import json
 _TEMPLATE = \
 {
   "@Redfish.Copyright": "Copyright 2014-2016 SNIA. All rights reserved.",
-  "@odata.context": "{rb}$metadata#StoragePool.StoragePool/{sp_id}",
-  "@odata.id": "{rb}StorageServices/{s_id}/StoragePool/{sp_id}",
+  "@odata.context": "{rb}$metadata#StoragePools.StoragePools/{sp_id}",
+  "@odata.id": "{rb}StorageServices/{s_id}/StoragePools/{sp_id}",
   "@odata.type": "#StoragePool_1_0_0.StoragePool",
   "Id": "{sp_id}",
   "Name": "",

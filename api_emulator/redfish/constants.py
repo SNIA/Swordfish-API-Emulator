d5886a30b039d69beefb6b72776c4cebc8ba390e
Richelle Ahlvers <richelle.ahlvers@intel.com>
2021-08-13 17:07:06 -0600

    Updated utils

--- api_emulator/redfish/constants.py
+++ api_emulator/redfish/constants.py
@@ -101,7 +101,6 @@ PATHS = {
         'ioperformance_los_capabilities': 'IOPerformanceLoSCapabilities/',
         'endpoints': 'Endpoints/',
         'endpoint_groups': 'EndpointGroups/',
-	    'storage_subsystems':'StorageSubsystems/',
         'file_systems': 'FileSystems/'
 
     },

0e980adedf628d62311f309afcb7a2ab077ddd15
Richelle Ahlvers <richelle.ahlvers@intel.com>
2021-08-16 22:01:07 -0600

    Update MemoryChunks

--- api_emulator/redfish/md_chunks_api.py
+++ api_emulator/redfish/md_chunks_api.py
@@ -27,56 +27,42 @@
 #  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
 #  THE POSSIBILITY OF SUCH DAMAGE.
 #
-# md_chunks_api.py
 
+# c_memory_api.py
 
 import json, os
 import traceback
 import logging
+import shutil
+
 import g
 import urllib3
-import shutil
 
 from flask import jsonify, request
 from flask_restful import Resource
-from api_emulator.utils import update_collections_json
+from api_emulator.utils import update_collections_json, create_path, get_json_data, create_and_patch_object, delete_object, patch_object, put_object, delete_collection, create_collection
 from .constants import *
 from .templates.md_chunks import get_MDChunks_instance
 
-
 members =[]
 member_ids = []
-foo = False
 config = {}
 INTERNAL_ERROR = 500
 
-
-
-def create_path(*args):
-    trimmed = [str(arg).strip('/') for arg in args]
-    return os.path.join(*trimmed)
-
-
-# MDChunksAPI API
+# MemoryDomainAPI API
 class MDChunksAPI(Resource):
     def __init__(self, **kwargs):
         logging.info('MDChunksAPI init called')
         self.root = PATHS['Root']
         self.chassis = PATHS['Chassis']['path']
-        self.memory_domains = PATHS['Chassis']['memory_domains']
+        self.memory_domains = PATHS['Chassis']['memory_domain']
         self.md_chunks = PATHS['Chassis']['md_chunks']
+
 
     # HTTP GET
     def get(self, chassis, memory_domain, md_chunks):
         path = create_path(self.root, self.chassis, chassis, self.memory_domains, memory_domain, self.md_chunks, md_chunks, 'index.json')
-        logging.info('Path 3 ==' + path)
-        try:
-            md_chunks_json = open(path)
-            data = json.load(md_chunks_json)
-        except Exception as e:
-            traceback.print_exc()
-            raise Exception("Unable read file because of following error::{}".format(e))
-        return jsonify(data)
+        return get_json_data (path)
 
     # HTTP POST
     # - Create the resource (since URI variables are available)
@@ -84,193 +70,93 @@ class MDChunksAPI(Resource):
     # - Attach the APIs of subordinate resources (do this only once)
     # - Finally, create an instance of the subordiante resources
     def post(self, chassis, memory_domain, md_chunks):
-        logging.info('MDChunksAPI PUT called')
+        logging.info('MDChunksAPI POST called')
+        path = create_path(self.root, self.chassis, chassis, self.memory_domains, memory_domain, self.md_chunks, md_chunks)
+        collection_path = os.path.join(self.root, self.chassis, chassis, self.memory_domains, memory_domain, self.md_chunks, 'index.json')
+
+        # Check if collection exists:
+        if not os.path.exists(collection_path):
+            MDChunksCollectionAPI.post (self, chassis, memory_domain)
+
+        if md_chunks in members:
+            resp = 404
+            return resp
         try:
             global config
-            global foo
-
-            wildcards = {'c_id':chassis,'n_id': memory_domain, 'p_id': md_chunks, 'rb': g.rest_base}
+            wildcards = {'c_id':chassis, 'md_id': memory_domain, 'mc_id': md_chunks, 'rb': g.rest_base}
             config=get_MDChunks_instance(wildcards)
+            config = create_and_patch_object (config, members, member_ids, path, collection_path)
 
-            members.append(config)
-            member_ids.append({'@odata.id': config['@odata.id']})
-
-            # Create instances of subordinate resources, then call put operation
-            # not implemented yet
-
-            path = create_path(self.root, self.chassis, chassis, self.memory_domains, memory_domain, self.md_chunks, md_chunks)
-            if not os.path.exists(path):
-                os.mkdir(path)
-            else:
-                # This will execute when POST is called for more than one time for a resource
-                return config, 500
-            with open(os.path.join(path, "index.json"), "w") as fd:
-                fd.write(json.dumps(config, indent=4, sort_keys=True))
-
-
-            # update the collection json file with new added resource
-            collection_path = os.path.join(self.root, self.chassis, chassis, self.memory_domains, memory_domain, self.md_chunks, 'index.json')
-            update_collections_json(path=collection_path, link=config['@odata.id'])
-
-
-
+            # Create sub-collections:
             resp = config, 200
+
         except Exception:
             traceback.print_exc()
             resp = INTERNAL_ERROR
-        logging.info('MDChunksAPI put exit')
+        logging.info('MDChunksAPI POST exit')
         return resp
+
 	# HTTP PATCH
     def patch(self, chassis, memory_domain, md_chunks):
         path = os.path.join(self.root, self.chassis, chassis, self.memory_domains, memory_domain, self.md_chunks, md_chunks, 'index.json')
-        try:
-            # Read json from file.
-            with open(path, 'r') as md_chunks_json:
-                data = json.load(md_chunks_json)
-                md_chunks_json.close()
+        patch_object(path)
+        return self.get(chassis, memory_domain)
 
-            request_data = json.loads(request.data)
-
-            if request_data:
-                # Update the keys of payload in json file.
-                for key, value in request_data.items():
-                    if key in data and data[key]:
-                        data[key] = value
-
-            # Write the updated json to file.
-            with open(path, 'w') as f:
-                json.dump(data, f)
-                f.close()
-
-        except Exception as e:
-            return {"error": "Unable read file because of following error::{}".format(e)}, 500
-
-        json_data = self.get(chassis, memory_domain, md_chunks)
-        return json_data
+    # HTTP PUT
+    def put(self, chassis, memory_domain, md_chunks):
+        path = os.path.join(self.root, self.chassis, chassis, self.memory_domains, memory_domain, self.md_chunks, md_chunks, 'index.json')
+        put_object(path)
+        return self.get(chassis, memory_domain)
 
     # HTTP DELETE
     def delete(self, chassis, memory_domain, md_chunks):
-
-        path = os.path.join(self.root, self.chassis, chassis, self.memory_domains, memory_domain, self.md_chunks, md_chunks).replace("\\","/")
-        print (path)
-        delPath = path.replace('Resources','/redfish/v1')
-        path2 = os.path.join(self.root, self.chassis, chassis, self.memory_domains, memory_domain, self.md_chunks, 'index.json').replace("\\","/")
-        try:
-            with open(path2,"r") as pdata:
-                pdata = json.load(pdata)
-
-            data = {
-            "@odata.id":delPath
-            }
-            resp = 200
-            jdata = data["@odata.id"].split('/')
-
-            path1 = os.path.join(self.root, self.chassis, chassis, self.memory_domains, memory_domain, self.md_chunks, jdata[len(jdata)-1])
-
-            shutil.rmtree(path1)
-            pdata['Members'].remove(data)
-            pdata['Members@odata.count'] = int(pdata['Members@odata.count']) - 1
-
-            with open(path2,"w") as jdata:
-                json.dump(pdata,jdata)
+        #Set path to object, then call delete_object:
+        path = create_path(self.root, self.chassis, chassis, self.memory_domains, memory_domain, self.md_chunks, md_chunks)
+        base_path = create_path(self.root, self.chassis, chassis, self.memory_domains, memory_domain, self.md_chunks)
+        return delete_object(path, base_path)
 
 
-        except Exception as e:
-            return {"error": "Unable read file because of following error::{}".format(e)}, 500
-
-        return jsonify(resp)
-
-
-# MDChunks Collection API
+# MemoryDomains Collection API
 class MDChunksCollectionAPI(Resource):
 
     def __init__(self):
-        logging.info('MDChunksCollectionAPI init called')
         self.root = PATHS['Root']
         self.chassis = PATHS['Chassis']['path']
-        self.memory_domains = PATHS['Chassis']['memory_domains']
+        self.memory_domains = PATHS['Chassis']['memory_domain']
         self.md_chunks = PATHS['Chassis']['md_chunks']
 
     def get(self, chassis, memory_domain):
-        path = os.path.join(self.root, self.chassis, chassis, self.memory_domains, memory_domain, self.md_chunks, 'index.json')
-        logging.info('Path 1 ==' + path)
-        try:
-            storage_controller_json = open(path)
-            data = json.load(storage_controller_json)
-        except Exception as e:
-            traceback.print_exc()
-            return {"error": "Unable read file because of following error::{}".format(e)}, 500
-
-        return jsonify(data)
+        path = os.path.join(self.root, self.chassis, chassis, self.memory_domains, 'index.json')
+        return get_json_data (path)
 
     def verify(self, config):
         # TODO: Implement a method to verify that the POST body is valid
         return True,{}
 
-    # HTTP POST
-    # POST should allow adding multiple instances to a collection.
-    # For now, this only adds one instance.
-    # TODO: 'id' should be obtained from the request data.
+    # HTTP POST Collection
     def post(self, chassis, memory_domain):
-        logging.info('MDChunksCollectionAPI POST called')
-        try:
-            config = request.get_json(force=True)
-            ok, msg = self.verify(config)
-            if ok:
-                # Save the new singleton
-                singleton_name = os.path.basename(config['@odata.id'])
-                path = os.path.join(self.root, self.chassis, chassis, self.memory_domains, memory_domain, self.md_chunks, singleton_name)
-                if not os.path.exists(path):
-                    os.mkdir(path)
-                with open(os.path.join(path, "index.json"), "w") as fd:
-                    fd.write(json.dumps(config, indent=4, sort_keys=True))
-                # Update the collection
-                collection_path = os.path.join(self.root, self.chassis, chassis, self.memory_domains, memory_domain, self.md_chunks, 'index.json')
-                update_collections_json(collection_path, config['@odata.id'])
-                # Return a copy of the new singleton with a Created response
-                resp = config, 201
-            else:
-                resp = msg, 400
-        except Exception:
-            traceback.print_exc()
-            resp = INTERNAL_ERROR
-        return resp
-
-
-class CreateMDChunk (Resource):
-    def __init__(self):
         self.root = PATHS['Root']
         self.chassis = PATHS['Chassis']['path']
-        self.memory_domains = PATHS['Chassis']['memory_domains']
-        self.md_chunks = PATHS['Chassis']['md_chunks']
+        self.memory_domains = PATHS['Chassis']['memory_domain']
 
-    # Attach APIs for subordinate resource(s). Attach the APIs for a resource collection and its singletons
-    def put(self,chassis, memory_domain):
-        logging.info('CreateMDChunk put started.')
-        try:
-            path = create_path(self.root, self.chassis, chassis, self.memory_domains, memory_domain, self.md_chunks)
-            logging.info('Path 2 ==' + path)
-            if not os.path.exists(path):
-                os.mkdir(path)
-            else:
-                logging.info('The given path : {} already Exist.'.format(path))
-            config={
-                      "@Redfish.Copyright": "Copyright 2015-2021 SNIA. All rights reserved.",
-                      "@odata.type": "#PortCollection.PortCollection",
-                      "Name": "Port Collection",
-                      "Members@odata.count": 0,
-                      "Members": [
-                      ],
-                      "Permissions": [
-                                {"Read": "True"},
-                                {"Write": "True"}]
-                    }
-            with open(os.path.join(path, "index.json"), "w") as fd:
-                fd.write(json.dumps(config, indent=4, sort_keys=True))
+        logging.info('MDChunksCollectionAPI POST called')
 
-            resp = config, 200
-        except Exception:
-            traceback.print_exc()
-            resp = INTERNAL_ERROR
-        logging.info('CreateMDChunk put exit.')
-        return resp
+        if memory_domain in members:
+            resp = 404
+            return resp
+
+        path = create_path(self.root, self.chassis, chassis, self.memory_domains, memory_domain, self.md_chunks)
+        return create_collection (path, 'MemoryChunk')
+
+    # HTTP PUT
+    def put(self, chassis, memory_domain):
+        path = os.path.join(self.root, self.chassis, chassis, self.memory_domains, memory_domain, self.md_chunks, 'index.json')
+        put_object(path)
+        return self.get(chassis)
+
+    # HTTP DELETE
+    def delete(self, chassis, memory_domain):
+        #Set path to object, then call delete_object:
+        path = create_path(self.root, self.chassis, chassis, self.memory_domains, memory_domain, self.md_chunks)
+        base_path = create_path(self.root, self.chassis, chassis, self.memory_domains, memory_domain)
+        return delete_collection(path, base_path)

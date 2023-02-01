a9c2a3a8fd30c3b178c7f965c6bc718a5e861177
Christian Pinto <christian.pinto@ibm.com>
2023-02-01 11:26:10 +0000

    Improved GH actions support
    - Triggering now for each PR opened
    - Improved the way how the test script waits (or fails) for the OFMF to come up
    
    Signed-off-by: Christian Pinto <christian.pinto@ibm.com>

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
-set -x
+TIMEOUT=30
+
+wait_ofmf () {
+    NOW=0
+    ELAPSED=0
+    START=$(date +"%s")
+    while [ $ELAPSED -lt $TIMEOUT ]
+    do
+        curl http://localhost:5000/redfish/v1 > /dev/null 2>&1
+        if [ $? -eq 0 ]
+        then
+            return 0
+        fi
+        # No need to go crazy here, let's sleep for a while
+        sleep 1
+        NOW=$(date +"%s")
+        ELAPSED=$((${NOW} - ${START}))
+    done
+
+    return 1
+}
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 echo "Script dir: $SCRIPT_DIR"
@@ -11,7 +31,12 @@ cd ${SCRIPT_DIR}/../emul
 ./venv/bin/python emulator.py &
 cd -
 
-# Transform this into a loop waiting for the service to go up
-sleep 20
+# wait for the OFMF service to be up of fail after TIMEOUT seconds
+wait_ofmf
+if [ $? -eq 1 ]
+then
+    echo "Timing out, the OFMF failed to start"
+    exit 1
+fi
 
 python ${SCRIPT_DIR}/test.py
